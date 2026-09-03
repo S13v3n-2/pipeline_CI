f6c02734a38aa86f25b9f29160fa678038efba42
Carlot Steven <steven.carlotpro@gmail.com>
2026-09-03 17:01:24 +0200

    Upload pipeline artifact final

--- job.py
+++ job.py
@@ -1,3 +1,12 @@
+import os
+
 a = 2
 
 print("coucou", a)
+
+secret = os.getenv("SECRET_API_TOKEN")
+
+if secret:
+    print("Le secret est accessible au script Python")
+else:
+    print("Le secret n'est pas accessible")
